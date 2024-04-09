bd6cb22da2bbe975fdfd4290e2f19e45c39d830f
Kobe <dev.skyachieve91@gmail.com>
2024-04-09 09:04:27 +0900

    :sparkles: Feat: 'String 클래스 - 불변 객체' 학습.

--- java-mid1/src/lang/string/equals/StringEqualsMain1.java
+++ java-mid1/src/lang/string/equals/StringEqualsMain1.java
@@ -3,8 +3,8 @@ package lang.string.equals;
 public class StringEqualsMain1 {
 
   public static void main(String[] args) {
-    String str1 = new String("hello");
-    String str2 = new String("hello");
+    String str1 = new String("hello"); // x001
+    String str2 = new String("hello"); // x002
     System.out.println("new String() == 비교: " + (str1 == str2));
     System.out.println("new String() equals 비교:" + (str1.equals(str2)));
 
