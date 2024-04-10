50df61dce2024b2e3cd429f90d077a20ca39d192
Kobe <dev.skyachieve91@gmail.com>
2024-04-10 12:49:40 +0900

    :sparkles: Feat: 'StringBuilder - 가변 String' 학습.

---
+++ java-mid1/src/lang/string/builder/StringBuilderMain1_1.java
@@ -0,0 +1,26 @@
+package lang.string.builder;
+
+public class StringBuilderMain1_1 {
+
+  public static void main(String[] args) {
+    StringBuilder sb = new StringBuilder();
+    sb.append("A");
+    sb.append("B");
+    sb.append("C");
+    sb.append("D");
+    System.out.println("sb = " + sb);
+
+    sb.insert(4, "Java");
+    System.out.println("sb = " + sb);
+
+    sb.delete(4,8);
+    System.out.println("delete = " + sb);
+
+    sb.reverse();
+    System.out.println("reverse = " + sb);
+
+    // StringBuilder(가변) -> String(불변)으로 바꿀 수 있음.
+    String string = sb.toString();
+    System.out.println("string = " + string);
+  }
+}
