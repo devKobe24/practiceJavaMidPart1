e9f9d22fa88d1b01f8901e87f25e77fc095378b3
Kobe <dev.skyachieve91@gmail.com>
2024-04-13 11:05:20 +0900

    :sparkles: Feat: 'String 클래스 - 문제와 풀이 6번 복습'

---
+++ java-mid1/src/lang/string/test/TestString7.java
@@ -0,0 +1,27 @@
+package lang.string.test;
+
+public class TestString7 {
+
+  public static void main(String[] args) {
+    String str = "start hello java, hello spring, hello jpa";
+    String key = "hello";
+
+    int count = 0;
+    int index = 0;
+    index = str.indexOf(key);
+    System.out.println("OUT OF WHILE ========== index = " + index);
+    System.out.println("OUT OF WHILE ========== count = " + count);
+    System.out.println();
+
+    while (index >= 0) {
+      index = str.indexOf(key, index + 1);
+      System.out.println("INSIDE OF WHILE ========== index = " + index);
+      count++;
+      System.out.println("INSIDE OF WHILE ========== count = " + count);
+      System.out.println();
+    }
+    System.out.println("END OF WHILE ========== index = " + index);
+    System.out.println("END OF WHILE ========== count = " + count);
+  }
+
+}
